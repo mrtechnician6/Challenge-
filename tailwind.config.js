f19e34e6bd640ad4ce569227c0819359aa8d10bc
Mr technician <yoshpick22@gmail.com>
2026-03-03 12:27:53 +0545

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: '#F97316', // High-viz Orange
+        dark: '#020617',  // Deep Navy/Black
+      },
+      fontFamily: {
+        'display': ['Inter', 'system-ui'], // Sharp, modern font
+      }
+    }
+  }
+}
